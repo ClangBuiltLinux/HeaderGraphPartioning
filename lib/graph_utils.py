379b892fda8e0602b529c61e77b2e4390dcd81cd
Tanzir Hasan <tanzirhasanhasan@gmail.com>
2023-11-30 23:58:31 +0000

    Graph Utility function for parsing AST currently working on not including all headers

--- lib/graph_utils.py
+++ lib/graph_utils.py
@@ -6,24 +6,30 @@ def extract_symbols_from_file(filename: Path, flags: List[str], header = False) 
     '''Obtains all the ast-nodes from filename built with flags'''
     index = Index.create()
     translation_unit = index.parse(str(filename), args = flags)
+    seentwice = set()
     symbols = set()
-    skip_headers = True
+
+    usable_types = {
+        CursorKind.STRUCT_DECL, CursorKind.ENUM_DECL, CursorKind.TYPEDEF_DECL,
+        CursorKind.CLASS_DECL, CursorKind.VAR_DECL, CursorKind.FUNCTION_DECL,
+        CursorKind.MACRO_DEFINITION, CursorKind.FIELD_DECL
+    }
+
     def visit_node(node):
-        nonlocal skip_headers
         '''Recursively crawls AST and adds relevant nodes'''
-        if ((not header and node.kind in {
-            CursorKind.STRUCT_DECL, CursorKind.ENUM_DECL, CursorKind.TYPEDEF_DECL,
-            CursorKind.CLASS_DECL, CursorKind.VAR_DECL, CursorKind.FUNCTION_DECL,
-            CursorKind.MACRO_DEFINITION, CursorKind.FIELD_DECL
-        } and (not skip_headers or (".h" not in str(node.location.file)))) or (header and node.kind in{
-            CursorKind.STRUCT_DECL, CursorKind.ENUM_DECL, CursorKind.TYPEDEF_DECL,
-            CursorKind.CLASS_DECL, CursorKind.VAR_DECL, CursorKind.FUNCTION_DECL,
-            CursorKind.MACRO_DEFINITION, CursorKind.FIELD_DECL
-        })):
-            skip_headers = False
+        if not header and node.kind in usable_types:
+            name = node.spelling
+            if ".c" in str(node.location.file) or name in symbols:
+                seentwice.add(name)
+            else:
+                symbols.add(name)
+
+        if header and node.kind in usable_types:
             name = node.spelling
             symbols.add(name)
+
         for child in node.get_children():
             visit_node(child)
+
     visit_node(translation_unit.cursor)
     return symbols
