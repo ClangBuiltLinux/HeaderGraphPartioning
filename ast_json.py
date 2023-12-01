9da1e2265c37530b08c76c5a68e70cba81f7451f
Tanzir Hasan <tanzirhasanhasan@gmail.com>
2023-12-01 17:02:14 +0000

    Added main incomplete workflow

---
+++ ast_json.py
@@ -0,0 +1,36 @@
+import argparse
+from collections import defaultdict
+from pathlib import Path
+from typing import Dict, List
+from pydantic import BaseModel
+
+class DataModel(BaseModel):
+    data: Dict[str, List[str]]
+
+
+def parse_file(file_path: Path) -> Dict[str, List[str]]:
+    inverted_dict = defaultdict(list)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            data = line.strip().split(' : ')
+            key, values_str = data[0], ' '.join(data[1:])
+            values = values_str.strip('{}').split(',')
+            for value in values:
+                value = value.strip()
+                inverted_dict[value].append(key)
+    return inverted_dict
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='''This script formats an ast-dump from
+                                     process_ast.py into a json file.''')
+    parser.add_argument('-f', '--file', type=Path, required=True,
+                        help='Path to the ast_dump')
+    parser.add_argument('-o', '--outfile', type=Path, default="outfile.json",
+                        help='Name of the output json')
+    args = parser.parse_args()
+    parsed_data = parse_file(args.file)
+    model = DataModel(data=parsed_data)
+    json_data = model.json()
+    with open(args.outfile, 'w', encoding='utf-8') as out_file:
+        out_file.write(json_data)
---
+++ hierarchical_agglomeration.py
@@ -0,0 +1,70 @@
+import sys
+from collections import defaultdict
+import json
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+import matplotlib.pyplot as plt
+import numpy as np
+
+from lib.graph_utils import extract_symbols_from_file
+
+def compute_proximity(header_filename, c_filenames):
+    with open(c_filenames) as json_data:
+        header_symbols = extract_symbols_from_file(header_filename, [], header=True)
+        proximity = defaultdict(int)
+        data = json.load(json_data)["data"]
+        for sym1 in header_symbols:
+            for sym2 in header_symbols:
+                if sym1 != sym2 and sym1 in data and sym2 in data:
+                    pairings = set(data[sym1]).intersection(set(data[sym2]))
+                    if pairings:
+                        proximity[(sym1, sym2)] = len(pairings)
+    return proximity
+
+def create_distance_matrix(proximity, symbols):
+    n = len(symbols)
+    LARGE_VALUE = 2
+    matrix = np.full((n, n), LARGE_VALUE)
+    print(symbols, n)
+    
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                matrix[i][j] = 0
+            else:
+                pair = (symbols[i], symbols[j])
+                if pair in proximity:
+                    matrix[i][j] = 1 / proximity[pair]
+
+    return matrix
+
+
+def hierarchical_clustering(proximity):
+    symbols = list(extract_symbols_from_file(header_filename, [], header=True))
+    distance_matrix = create_distance_matrix(proximity, symbols)
+    linked = linkage(distance_matrix, method='average')
+    
+    plt.figure(figsize=(10, 7))
+    dendrogram(linked, orientation='top', labels=symbols, distance_sort='descending')
+    plt.show()
+
+    clusters = fcluster(linked, 2, criterion='maxclust')
+    symbol_to_cluster = {symbols[i]: cluster_id for i, cluster_id in enumerate(clusters)}
+
+    return symbol_to_cluster
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(f"Usage: {sys.argv[0]} <header_filename> <compile_commands.json>")
+        sys.exit(1)
+
+    header_filename = sys.argv[1]
+    c_filenames = sys.argv[2]
+    proximity = compute_proximity(header_filename, c_filenames)
+
+    sorted_data = sorted([(count, sym1, sym2) for (sym1, sym2), count in proximity.items()], reverse=True)
+    for count, sym1, sym2 in sorted_data:
+        print(f"Proximity SYM:{sym1} - SYM:{sym2}: {count}")
+
+    clusters = hierarchical_clustering(proximity)
+    for symbol, cluster_id in clusters.items():
+        print(f"Symbol {symbol} is in cluster {cluster_id}")
