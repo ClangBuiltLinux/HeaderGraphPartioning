a62dd8140409c33bc53f65874d35da141c29544a
Tanzir Hasan <tanzirhasanhasan@gmail.com>
2023-12-12 17:43:14 +0000

    merged ast_json and process_ast

--- ast_json.py
+++ ast_json.py
@@ -1,37 +1,57 @@
 import argparse
 from collections import defaultdict
+import json
 from pathlib import Path
-from typing import Dict, List
+import os
+from typing import Dict, List, Tuple
 from pydantic import BaseModel
+
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from lib.graph_utils import extract_symbols_from_file
 
 class DataModel(BaseModel):
     data: Dict[str, List[str]]
 
+def process_file(row: Dict[str, str]) -> str:
+    c_file = Path(row["file"])
+    flags = row["command"].split()[1:-4]
+    os.chdir(row["directory"])
+    c_file_symbols = extract_symbols_from_file(c_file, flags)
+    pieces = "linux".join(str(c_file).split("linux")[1:])
+    return (pieces, c_file_symbols)
 
-def parse_file(file_path: Path) -> Dict[str, List[str]]:
+def compute_usage(c_commands: Path):
+    result = []
+    with open(c_commands, encoding="utf-8") as json_data:
+        data = json.load(json_data)
+        rows = list(data)
+        workers = len(os.sched_getaffinity(0))
+        with ProcessPoolExecutor(max_workers = workers // 2 + 1) as executor:
+            futures = [executor.submit(process_file, row) for row in rows]
+            for future in as_completed(futures):
+                result.append(future.result())
+    return result
+
+def parse_file(data: List[Tuple[str, str]]) -> Dict[str, List[str]]:
     inverted_dict = defaultdict(list)
-    with open(file_path, "r", encoding="utf-8") as file:
-        for line in file:
-            data = line.strip().split(" : ")
-            key, values_str = data[0], " ".join(data[1:])
-            values = values_str.strip("{}").split(",")
-            for value in values:
-                value = value.strip()
-                if value == "":
-                    continue
-                inverted_dict[value].append(key)
+    for file, symbols in data:
+        for value in symbols:
+            value = value.strip()
+            if value == "":
+                continue
+            inverted_dict[value].append(file)
     return inverted_dict
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='''This script formats an ast-dump from
                                      process_ast.py into a json file.''')
-    parser.add_argument("-f", "--file", type=Path, required=True,
-                        help="Path to the ast_dump")
+    parser.add_argument('-c', '--commands', type=Path, required=True,
+                        help='Path to compile_commands.json')
     parser.add_argument("-o", "--outfile", type=Path, default="outfile.json",
                         help="Name of the output json")
     args = parser.parse_args()
-    parsed_data = parse_file(args.file)
+    parsed_data = parse_file(compute_usage(args.commands))
     model = DataModel(data=parsed_data)
     json_data = model.json()
     with open(args.outfile, "w", encoding="utf-8") as out_file:
